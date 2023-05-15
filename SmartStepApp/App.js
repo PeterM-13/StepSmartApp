9d54b865fdd50804fa5d62cbdb63f86ea74c3daa
Peter <petershotbox@gmail.com>
2023-05-15 12:21:52 +0100

    main app clean up

--- SmartStepApp/App.js
+++ SmartStepApp/App.js
@@ -1,5 +1,3 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
-//import Navigator from "./routes/homeStack";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import StartPage from './components/StartPage';
@@ -20,13 +18,4 @@ export default function App() {
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  }
