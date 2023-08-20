a89646cd92081faccde1e4569c3e0f3685e53cbf
Peter <petershotbox@gmail.com>
2023-08-20 15:41:23 +0100

    Notifications working and updated app icon

--- SmartStepApp/App.js
+++ SmartStepApp/App.js
@@ -1,5 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useState, useEffect, useRef } from 'react';
+import * as Notifications from 'expo-notifications';
 
 import LogInPage from './components/LogInPage';
 import UserHomePage from './components/UserHomePage';
@@ -11,10 +13,59 @@ import LostPage from './components/LostPage';
 import HeartPage from './components/HeartPage';
 import SettingsPage from './components/SettingsPage';
 
+import {sendPushNotification, registerForPushNotificationsAsync} from './components/NotificationsHandler';
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   global.textSize = 0;
+
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+    //console.log('myToken: "', expoPushToken, '"');
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+    
+  }, []);
+
+  // useEffect(()=>{
+  //   const sendData = async () => {
+  //     try {
+  //         const response = await fetch(`https://stepsmartapi.onrender.com/StepSmart/api/devices?code=${global.code}`, {
+  //             method: 'PATCH',
+  //             headers: {
+  //                 'Content-Type': 'application/json'
+  //             },
+  //             body: JSON.stringify({lost: lost})
+  //         });
+  //     if (response.ok) {
+  //         console.log('Lost data sent successfully!');
+  //     } else {
+  //         console.error('Failed to send alert data. Status:', response.status);
+  //     }
+  //     } catch (error) {
+  //         console.error('Error occurred while sending alert data:', error);
+  //     }
+  // };
+  // sendData();
+  // }, [expoPushToken])
+  
+
   return (
     // screen navigation stack
     <NavigationContainer>
